6efcc82773ce3a397f9911e3e8a9ba1a1c18a302
Sam Saccone <samccone@google.com>
2016-10-27 20:52:39 -0700

    PWA progress

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js').then(_ => {
+    console.log('service worker is all cool 🐳');
+  }).catch(e => {
+    console.error('service worker is not so cool 🔥', e);
+    throw e;
+  });
+
+
+  if (navigator.serviceWorker.controller) {
+    // Correctly prompt the user to reload during SW phase change.
+    navigator.serviceWorker.controller.onstatechange = e => {
+      if (e.target.state === 'redundant') {
+        document.querySelector('#reload-prompt').style.visibility = 'visible';
+      }
+    }
+  }
+}
+
 var $ = document.querySelector.bind(document);
 
 var recordButton = $('#record');
